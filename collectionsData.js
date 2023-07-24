2d866a593593087e2cb4d147c1059b72b225b4c3
dogteeth <dogteeth@gmail.com>
2023-07-24 20:24:05 +0800

    json file import test

---
+++ collectionsData.js
@@ -0,0 +1,2 @@
+console.log("collection 01 file directory is good");
+export const data = ["apple", "banana", "orange"];
---
+++ collectionsDataImport.js
@@ -0,0 +1,4 @@
+console.log("js 02 is good");
+import { data } from "./collectionsData.js";
+
+console.log(data[0]);
